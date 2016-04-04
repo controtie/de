190a361fec34e123de000f11110dfb9a8ebd4be8
controtie <dylan.ad.tran@gmail.com>
2016-04-04 14:28:09 -0700

    modify server for serving static files in src

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ var app = express();
 app.use(parser.json());
 app.use(parser.urlencoded({extended: false}));
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 //   app.set('views', __dirname + '/views');
 //   app.set('view engine', 'jade');
 //   app.use(express.favicon());
@@ -18,14 +17,15 @@ app.configure(function(){
 //   app.use(express.bodyParser());
 //   app.use(express.methodOverride());
 //   app.use(app.router);
-//   app.use(express.static(path.join(__dirname, 'public')));
-});
 
 // app.configure('development', function(){
 //   app.use(express.errorHandler());
 // });
+app.get('/', function (req, res) {
+  res.redirect('index.html');
+});
 
-app.get('/', 'index.html');
+app.use(express.static(path.join(__dirname, 'src')));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
